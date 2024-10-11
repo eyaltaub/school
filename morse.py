2f1b6ef19f09516bd5972f228d909df30baccfb2
eyaltaub <taubeyal@gmail.com>
2024-10-11 13:08:55 +0300

    project 2

--- morse.py
+++ morse.py
@@ -1,5 +1,5 @@
 
-def isMorse(letter):
+def is_morse(letter):
     morsedict = {'A': ".-", 'B': "-...", 'C': "-.-.", 'D': "-..", 'E': ".", 'F': "..-.", 'G': "--.", 'H': "....",
                  'I': "..", 'J': ".---", 'K': "-.-", 'L': ".-..", 'M': "--", 'N': "-.", 'O': "---", 'P': ".--.",
                  'Q': "--.-", 'R': ".-.", 'S': "...", 'T': "-", 'U': "..-", 'V': "...-", 'W': ".--", 'X': "-..-",
@@ -15,13 +15,13 @@ def isMorse(letter):
 def morse_file(file):
     unencrypted = ''
     with open(file, 'r') as file:
-        fileString = file.read().replace('\n', '')
-    fileList = fileString.split(' / ')
-    for word in fileList:
+        file_string = file.read().replace('\n', '')
+    file_list = file_string.split(' / ')
+    for word in file_list:
         letters = word.split(' ')
         for letter in letters:
-            if isMorse(letter) != "wrong":
-                unencrypted = unencrypted + isMorse(letter)
+            if is_morse(letter) != "wrong":
+                unencrypted = unencrypted + is_morse(letter)
             else:
                 return "Error in Morse Code"
         unencrypted = unencrypted + ' '
@@ -30,24 +30,24 @@ def morse_file(file):
 
 
 def count_symbols(file):
-    symbolsDict = {}
-    with open(file, 'r') as file:
-        fileString = file.read().replace('\n', '')
-        fileString = fileString.replace(' ', '')
-    while fileString != '':
-        symbolsDict[fileString[0]] = fileString.count(fileString[0])
-        fileString = fileString.replace(fileString[0], '')
-    sorted_dict = dict(sorted(symbolsDict.items(), key=lambda item: item[1], reverse=True))
+    symbols_dict = {}
+    file_string = morse_file(file)
+    file_string = file_string.replace(' ', '')
+    while file_string != '':
+        symbols_dict[file_string[0]] = file_string.count(file_string[0])
+        file_string = file_string.replace(file_string[0], '')
+    sorted_dict = dict(sorted(symbols_dict.items(), key=lambda item: item[1], reverse=True))
     return sorted_dict
 
 
 def print_count(file):
-    symbolsDict = count_symbols(file)
-    keyList = []
-    for item in symbolsDict:
-        keyList.append(symbolsDict[item])
-    keyList = sorted(list(set(keyList)), reverse=True)
-    print(keyList)
-    for item in keyList:
-        symbolsList = [i for i, j in symbolsDict.items() if j == item]
-        print("".join(symbolsList) + " - " + str(item))
+    symbols_dict = count_symbols(file)
+    key_list = []
+    for item in symbols_dict:
+        key_list.append(symbols_dict[item])
+    key_list = sorted(list(set(key_list)), reverse=True)
+    print(key_list)
+    for item in key_list:
+        symbols_list = [i for i, j in symbols_dict.items() if j == item]
+        print("".join(symbols_list) + " - " + str(item))
+
