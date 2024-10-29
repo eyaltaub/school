15d5a54e4eac36e7e8d67b9891bb81720e315685
eyaltaub <taubeyal@gmail.com>
2024-10-29 13:15:20 +0200

    binaryTree.py

---
+++ binaryTree.py
@@ -0,0 +1,35 @@
+import re
+
+
+def counter(node, character):
+    if node is None:
+        return 0
+    if character == node.value:
+        count = 1
+    else:
+        count = 0
+    count += counter(node.left, character)
+    count += counter(node.right, character)
+    return count
+
+
+def remove_consecutive_duplicates(s):
+    return re.sub(r'(.)\1+', r'\1', s)
+
+
+def all_in(node, string):
+    def values_to_set(node1):
+        if not node1:
+            return set()
+
+        values = {node1.value}
+        values.update(values_to_set(node1.left))
+        values.update(values_to_set(node1.right))
+        return values
+
+    values_set = values_to_set(node)
+    for char in string:
+        if char not in values_set:
+            return False
+    return True
+
